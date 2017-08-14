75a9a84faaa88c12686c2af0d37a5fb21e85cc91
supermin45 <1299657352@qq.com>
2017-08-14 16:55:56 +0800

    task8 compelete

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -8,24 +8,49 @@ chai.use(sinonChai);
 
 var main = require("../lib/main.js");
 
-
-describe("测试描述", function(){
+describe("PostNet encode and decode", function(){
     sinon.spy(console, 'log');
 
-    it("测试用例1", function(){
+    it("it returns the barcode string given the five digit post number", function(){
 
-        var result = main();
-        var expect_string = '';
+        var result = main('95713');
+        var expect_string = '| |:|:: :|:|: |:::| :::|| ::||: :|:|: |';
         
         expect(expect_string).to.equal(result);
     });
 
-    it("测试用例2", function(){
+    it("it returns the barcode string given the nine digit post number", function(){
 
-        main();
-        var result = _.flatten(console.log.args).join("\n");
-        var expect_string = '';
+        var result = main('957139571');
+        var expect_string = '| |:|:: :|:|: |:::| :::|| ::||: |:|:: :|:|: |:::| :::|| ::||: |';
 
         expect(expect_string).to.equal(result);
     });
+
+    it("it returns the barcode string given the ten digit post number", function(){
+
+        var result = main('95713-9571');
+        var expect_string = '| |:|:: :|:|: |:::| :::|| ::||: |:|:: :|:|: |:::| :::|| ::||: |';
+
+        expect(expect_string).to.equal(result);
+    });
+
+    it("it returns the post number given the six digit barcode string", function(){
+
+        let str = '| |:|:: :|:|: |:::| :::|| ::||: :|:|: |';
+        var result = main(str);
+        var expect_string = '95713';
+
+        expect(expect_string).to.equal(result);
+    });
+
+    it("it returns the post number given the ten digit barcode string", function(){
+
+        let str = '| |:|:: :|:|: |:::| :::|| ::||: |:|:: :|:|: |:::| :::|| ::||: |';
+        var result = main(str);
+        var expect_string = '95713-9571';
+
+        expect(expect_string).to.equal(result);
+    });
+
 });
